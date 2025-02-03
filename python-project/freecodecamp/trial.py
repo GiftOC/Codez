9098082c78477578d3b16b3d773efcabd1f0917b
GiftOC <giftnmn@gmail.com>
2025-02-03 17:30:41 +0000

    trial.py

--- python-project/freecodecamp/trial.py
+++ python-project/freecodecamp/trial.py
@@ -1,23 +1,35 @@
-'''password= input(str('Enter your passcode: '))
+def verify_cc(cc_number):
 
+      reversed_number = cc_number[::-1]
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-passcode = password
-encoder = 2
+      sum_single_numbers = 0
+      single_number = reversed_number[::2]
+      for digits in single_number:
+            sum_single_numbers += int(digits)
 
-def encoding():
+      sum_double_numbers = 0
+      double_number = reversed_number[1::2]
+      for digits in double_number:
+            number = int(digits)
+            if number > 9:
+                  number -= 9
+            sum_double_numbers += number
 
-      encrypted_pass = ''
-      for char in passcode.lower():
-            if char == ' ':
-                  encrypted_pass += char
-            elif char in alphabet:
-                  alphabet.find(char)
-                  index = alphabet.find(char)
-                  index += encoder % len(alphabet)
-                  encrypted_pass += alphabet[index]
-      print('plain passcode: ', passcode)
-      print('encrypted passcode: ', encrypted_pass)
-encoding()'''
+      total = sum_double_numbers + sum_single_numbers
+      return total % 10 == 0
 
+import re
+def cc_details():
+      cc_number = input("Enter your card number (xxxx-xxxx-xxxx-xxxx): ")
+
+      if not re.fullmatch(r'\d{4}-\d{4}-\d{4}\d{4}', cc_number):
+            print('invlid input, ensure card follows xxxx-xxxx-xxxx-xxxx')
+            return
+      card_reader = str.maketrans({'-': '', ' ': ''})
+      card_number = cc_number.translate(card_reader)
+
+      if verify_cc(card_number):
+            print('valid')
+      else:
+            print('invalid')
 
